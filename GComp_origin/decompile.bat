bffe92a42e72a7eed3bafb32ef39b7a24f358e1d
artamir <artamir@mail.ru>
2020-12-15 16:02:55 +0200

    Пустая база

--- GComp_origin/decompile.bat
+++ GComp_origin/decompile.bat
@@ -1,7 +1,7 @@
 @echo on
 
-copy /y .\release\baseprof\1cv7.md .\config\1cv7.cur.md >nul
-gcomp -d -v -F config\1cv7.cur.md -D SRC\MD > decompile.log
+copy /y ..\release\baseprof\1cv7.md ..\config\1cv7.cur.md >nul
+gcomp -d -v -F ..\config\1cv7.cur.md -D ..\SRC\MD > decompile.log
 echo MD decompiled
 
 rem cd _Модули
